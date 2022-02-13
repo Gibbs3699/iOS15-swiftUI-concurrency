ddf2b6204e618c5660a50ab9dca6e1ea293fa7e1
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2022-02-13 22:56:38 +0700

    Initial Commit

---
+++ iOS15-swiftUI-concurrency/iOS15_swiftUI_concurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS15_swiftUI_concurrencyApp.swift
+//  iOS15-swiftUI-concurrency
+//
+//  Created by admin on 13/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS15_swiftUI_concurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
